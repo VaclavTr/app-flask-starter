51ca783b2af39b577f11f2f558eef8f2353bacbd
Václav Trávníček <Travnicek.Vaclav@student.gymzr.cz>
2026-05-06 19:53:56 +0200

    validace inputů

--- app.py
+++ app.py
@@ -18,19 +18,26 @@ def pozdrav_post():
 	#aktuální datum
 	date = datetime.now().strftime("%d. %m. %Y")
 
-	name=request.form.get("name")
-	surname=request.form.get("surname")
-	password=request.form.get("password")
-	heslo = "SPRAVNE_HESLO"
+	name = request.form.get("name")
+	surname = request.form.get("surname")
+	password = request.form.get("password")
+	message_name1= None
+	message_name2 = None
+	if not name or not surname:
+		message_name1 = "ERROR: Zadej jméno a příjmení."
+	elif len(name) > 50 or len(surname) > 50:
+		message_name2 = "ERROR: Jméno a příjmení musí mít maximálně 50 znaků."
+		name = None
+		surname = None
 	message = None
-	
+	heslo = "SPRAVNE_HESLO"
 	if not password:
 		message=None
 	elif password == heslo:
 		message = "Správné heslo!"
 	else:
 		message = "ERROR: Chybné heslo!"
-	return render_template("pozdrav_post.html", date=date, name=name, surname=surname, message=message)
+	return render_template("pozdrav_post.html", date=date, name=name, surname=surname, message=message, message_name1=message_name1, message_name2=message_name2)	
 	
 
 if __name__=="__main__":
